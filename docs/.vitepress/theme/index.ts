5b694f63167c8aa9549ce6346a7c6eb8d00e7ee6
Osmand Lien <osmand8370@gmail.com>
2026-02-06 15:07:52 +0800

    docs: rename files

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,31 +1,31 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from "vue";
-import type { Theme } from "vitepress";
-import DefaultTheme from "vitepress/theme";
-import "./style.css";
-import "../theme/Customization/customizedStyle.scss";
-import "vuetify/styles";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
-import { createVuetify } from "vuetify";
+import { h } from "vue"
+import type { Theme } from "vitepress"
+import DefaultTheme from "vitepress/theme"
+import "./style.css"
+import "../theme/customization/customizedStyle.scss"
+import "vuetify/styles"
+import * as components from "vuetify/components"
+import * as directives from "vuetify/directives"
+import { createVuetify } from "vuetify"
 
 // pages
-import About from "./Customization/Pages/AboutPage.vue";
-import Projects from "./Customization/Pages/ProjectPage.vue";
+import About from "./customization/pages/AboutSection.vue"
+import Projects from "./customization/pages/ProjectsSection.vue"
 
-const vuetify = createVuetify({ components, directives });
+const vuetify = createVuetify({ components, directives })
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    });
+    })
   },
 
   enhanceApp({ app, router, siteData }) {
-    app.component("about", About);
-    app.component("projects", Projects);
-    app.use(vuetify);
+    app.component("about", About)
+    app.component("projects", Projects)
+    app.use(vuetify)
   },
-} satisfies Theme;
+} satisfies Theme
